2be43373925ef3e6df34a9d5434d668ff2fe12bd
Bobby Eshleman <eshleman@pdx.edu>
2017-09-25 15:10:01 -0700

    Initial commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Video from './Video.jsx';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        Video:
+        <Video />
+      </div>
+    )
+  }
+}
